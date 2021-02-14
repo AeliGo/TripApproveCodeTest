484a2d09fb5531deafd76132b57ab7d47ffd3367
zoey wang <38220838+AeliGo@users.noreply.github.com>
2021-02-14 13:44:05 +1100

    create save method to save user and clear form

--- src/components/Task2.vue
+++ src/components/Task2.vue
@@ -2,27 +2,26 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12" class="my-4">
-        <div class="text-h4 mb-3">
-          Task 2
-        </div>
+        <div class="text-h4 mb-3">Task 2</div>
 
         <div class="subheading font-weight-regular">
           We want to build a form to record user details.
 
           <div class="py-3">
-            Open the file <code>/src/components/Task2.vue</code> and modify the code below so that:
+            Open the file <code>/src/components/Task2.vue</code> and modify the
+            code below so that:
             <ol class="list-center">
               <li>
-                When I press on "save", the user information is saved and the form is cleared.
+                When I press on "save", the user information is saved and the
+                form is cleared.
               </li>
-              <li>
-                Any saved user is displayed on the right hand side table.
-              </li>
+              <li>Any saved user is displayed on the right hand side table.</li>
             </ol>
             Once completed, proceed to the next task.
           </div>
           <div class="text-caption">
-            Note: feel free to edit any of the component properties (data, methods, etc..)
+            Note: feel free to edit any of the component properties (data,
+            methods, etc..)
           </div>
         </div>
       </v-col>
@@ -32,17 +31,18 @@
     <v-row>
       <v-col cols="12" sm="6">
         <v-card class="pa-4">
-          <v-text-field label="First Name" />
-          <v-text-field label="Last Name" />
-          <v-btn color="primary">Save</v-btn>
+          <v-text-field label="First Name" v-model="form.first_name" />
+          <v-text-field label="Last Name" v-model="form.last_name" />
+          <v-btn color="primary" @click="handleSave">Save</v-btn>
         </v-card>
       </v-col>
       <v-col cols="12" sm="6">
         <v-data-table
           :headers="[
-            {text: 'First Name', value: 'firstName'},
-            {text: 'Last Name', value: 'lastName'},
+            { text: 'First Name', value: 'first_name' },
+            { text: 'Last Name', value: 'last_name' },
           ]"
+          :items="users"
           hide-default-footer
         />
       </v-col>
@@ -51,12 +51,22 @@
 </template>
 
 <script>
-  export default {
-    name: 'Task2',
-    data () {
-      return {}
+export default {
+  name: "Task2",
+
+  data() {
+    return {
+      form: {},
+      users: [],
+    };
+  },
+
+  methods: {
+    handleSave() {
+      const user = { ...this.form };
+      this.users.push(user);
+      this.form = {};
     },
-    methods: {
-    },
-  }
+  },
+};
 </script>
