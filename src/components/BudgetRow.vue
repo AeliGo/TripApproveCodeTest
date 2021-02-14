92b67bea9ab987afd4dae29159c889d1cc280420
zoey wang <38220838+AeliGo@users.noreply.github.com>
2021-02-14 14:08:16 +1100

    [Task4] component-budgetRow refactoring

--- src/components/BudgetRow.vue
+++ src/components/BudgetRow.vue
@@ -2,46 +2,52 @@
   <!-- EDIT CODE BELOW -->
   <v-row class="align-center">
     <v-col cols="2">
-      <v-btn icon small dark color="primary">
+      <v-btn icon small dark color="primary" @click.stop="$emit('deleted')">
         <v-icon> mdi-minus-circle </v-icon>
       </v-btn>
     </v-col>
     <v-col cols="5">
       <v-select
-        v-model="budgetType"
+        :value="type"
         label="Budget Type"
-        :items="budgetTypes"
+        :items="budgeTypes"
         :item-text="(item) => item.name"
         return-object
+        @input="(value) => $emit('update:type', value)"
       />
     </v-col>
     <v-col cols="5">
       <v-text-field
-        v-model="amount"
+        :value="amount"
         class="currency-input"
         label="Enter amount"
         type="number"
         prefix="$"
+        @input="(value) => $emit('update:amount', Number(value))"
       />
     </v-col>
   </v-row>
 </template>
 
 <script>
-import { loadBudgetTypes } from "@/mock.js";
-
 export default {
-  name: "BudgetRow",
-  data() {
-    return {
-      budgetType: null,
-      budgetTypes: [],
-      amount: 0,
-    };
+  name: "budget-row",
+
+  props: {
+    amount: {
+      type: Number,
+      default: 0,
+    },
+
+    type: {
+      type: Object,
+      default: null,
+    },
+
+    budgeTypes: {
+      type: Array,
+      default: () => [],
+    },
   },
-  created() {
-    this.budgetTypes = loadBudgetTypes();
-  },
-  methods: {},
 };
 </script>
