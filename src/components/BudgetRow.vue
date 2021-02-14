d62156bf71b3d283563217f5100d80a103eb54f4
zoey wang <38220838+AeliGo@users.noreply.github.com>
2021-02-14 13:58:39 +1100

    [Task4] vertically aligned row

--- src/components/BudgetRow.vue
+++ src/components/BudgetRow.vue
@@ -1,16 +1,9 @@
 <template>
   <!-- EDIT CODE BELOW -->
-  <v-row>
+  <v-row class="align-center">
     <v-col cols="2">
-      <v-btn
-        icon
-        small
-        dark
-        color="primary"
-      >
-        <v-icon>
-          mdi-minus-circle
-        </v-icon>
+      <v-btn icon small dark color="primary">
+        <v-icon> mdi-minus-circle </v-icon>
       </v-btn>
     </v-col>
     <v-col cols="5">
@@ -18,7 +11,7 @@
         v-model="budgetType"
         label="Budget Type"
         :items="budgetTypes"
-        :item-text="item => item.name"
+        :item-text="(item) => item.name"
         return-object
       />
     </v-col>
@@ -35,21 +28,20 @@
 </template>
 
 <script>
-  import { loadBudgetTypes } from '@/mock.js'
+import { loadBudgetTypes } from "@/mock.js";
 
-  export default {
-    name: 'BudgetRow',
-    data () {
-      return {
-        budgetType: null,
-        budgetTypes: [],
-        amount: 0,
-      }
-    },
-    created () {
-      this.budgetTypes = loadBudgetTypes()
-    },
-    methods: {
-    },
-  }
+export default {
+  name: "BudgetRow",
+  data() {
+    return {
+      budgetType: null,
+      budgetTypes: [],
+      amount: 0,
+    };
+  },
+  created() {
+    this.budgetTypes = loadBudgetTypes();
+  },
+  methods: {},
+};
 </script>
--- src/components/Task4.vue
+++ src/components/Task4.vue
@@ -2,46 +2,50 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12" class="my-4">
-        <div class="text-h4 mb-3">
-          Task 4
-        </div>
+        <div class="text-h4 mb-3">Task 4</div>
 
         <div class="subheading font-weight-regular">
           We want to add/remove travel budgets
 
           <div class="py-3">
             Open the following files:
-            <br> <code>/src/components/Task4.vue</code>
-            <br> <code>/src/components/BudgetRow.vue</code>
-            <br> and modify the code so that:
+            <br />
+            <code>/src/components/Task4.vue</code> <br />
+            <code>/src/components/BudgetRow.vue</code> <br />
+            and modify the code so that:
             <ol class="list-center">
               <li>
-                The <v-icon color="primary">mdi-minus-circle</v-icon> button is vertically aligned.
+                The <v-icon color="primary">mdi-minus-circle</v-icon> button is
+                vertically aligned.
               </li>
               <li>
-                When a budget type is selected and or an amount entered, the total budget is updated.
+                When a budget type is selected and or an amount entered, the
+                total budget is updated.
               </li>
               <li>
-                When the <v-icon color="primary">mdi-plus-circle</v-icon> button is pressed, it adds another BudgetRow.
+                When the <v-icon color="primary">mdi-plus-circle</v-icon> button
+                is pressed, it adds another BudgetRow.
               </li>
               <li>
-                When the <v-icon color="primary">mdi-minus-circle</v-icon> button is pressed, it removes that specific BudgetRow and updates the total budget.
+                When the
+                <v-icon color="primary">mdi-minus-circle</v-icon> button is
+                pressed, it removes that specific BudgetRow and updates the
+                total budget.
               </li>
             </ol>
             Once completed, proceed to the next task.
           </div>
           <div class="text-caption">
-            Note: feel free to edit any of the component properties (data, methods, etc..), css
+            Note: feel free to edit any of the component properties (data,
+            methods, etc..), css
           </div>
         </div>
       </v-col>
 
       <v-col cols="12">
-      <!-- EDIT CODE BELOW -->
+        <!-- EDIT CODE BELOW -->
         <v-card>
-          <v-card-subtitle>
-            Total budget: ${{ totalBudgets }}
-          </v-card-subtitle>
+          <v-card-subtitle> Total budget: ${{ totalBudgets }} </v-card-subtitle>
 
           <v-card-text>
             <BudgetRow />
@@ -49,12 +53,7 @@
 
           <v-card-actions>
             <v-spacer />
-            <v-btn
-              fab
-              dark
-              small
-              color="primary"
-            >
+            <v-btn fab dark small color="primary">
               <v-icon>mdi-plus</v-icon>
             </v-btn>
           </v-card-actions>
@@ -65,19 +64,18 @@
 </template>
 
 <script>
-  import BudgetRow from './BudgetRow'
+import BudgetRow from "./BudgetRow";
 
-  export default {
-    name: 'Task4',
-    components: {
-      BudgetRow,
-    },
-    data () {
-      return {
-        totalBudgets: 0
-      }
-    },
-    methods: {
-    },
-  }
+export default {
+  name: "Task4",
+  components: {
+    BudgetRow,
+  },
+  data() {
+    return {
+      totalBudgets: 0,
+    };
+  },
+  methods: {},
+};
 </script>
